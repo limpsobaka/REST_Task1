ebb528e0a58bc3419be8da7f0f92783deec7cfd1
Limpsobaka <limpsobaka@yandex.ru>
2023-02-20 22:27:28 +0300

    Change model, repo

--- src/main/java/ru/netology/rest/model/User.java
+++ src/main/java/ru/netology/rest/model/User.java
@@ -1,18 +1,24 @@
 package ru.netology.rest.model;
 
+import ru.netology.rest.enums.Authorities;
+
+import java.util.List;
+import java.util.Objects;
+
 public class User {
-  private String user;
+  private String userName;
   private String password;
+  private List<Authorities> authorities;
 
   public User() {
   }
 
-  public String getUser() {
-    return user;
+  public String getUserName() {
+    return userName;
   }
 
-  public void setUser(String user) {
-    this.user = user;
+  public void setUserName(String userName) {
+    this.userName = userName;
   }
 
   public String getPassword() {
@@ -21,5 +27,26 @@ public class User {
 
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  public List<Authorities> getAuthorities() {
+    return authorities;
+  }
+
+  public void setAuthorities(Authorities authorities) {
+    this.authorities.add(authorities);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    User user1 = (User) o;
+    return Objects.equals(userName, user1.userName) && Objects.equals(password, user1.password) && Objects.equals(authorities, user1.authorities);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(userName, password, authorities);
   }
 }
--- src/main/java/ru/netology/rest/repository/UserRepository.java
+++ src/main/java/ru/netology/rest/repository/UserRepository.java
@@ -4,22 +4,19 @@ import org.springframework.stereotype.Repository;
 import ru.netology.rest.enums.Authorities;
 import ru.netology.rest.model.User;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+
 @Repository
 public class UserRepository {
-    private HashMap<String, String> userList;
-    private HashMap<User, List<Authorities>> authoritiesList;
+    private final HashMap<String, User> userList;
 
     public UserRepository() {
         userList = new HashMap<>();
-        authoritiesList = new HashMap<>();
     }
 
     public List<Authorities> getUserAuthorities(String user, String password) {
-        if (userList.get(user) != null && userList.get(user).equals(password)) {
-            return authoritiesList.get(userList.get(user));
+        if (userList.containsKey(user) && userList.get(user).getPassword().equals(password)) {
+            return userList.get(user).getAuthorities();
         } else {
             return Collections.emptyList();
         }
